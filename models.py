dedace03406a3d50464fecdf7e9cad2a313445ce
Rémy Damasceno <damascre@merck.com>
2022-10-24 16:44:48 +0200

    Adding constructor and operator magic methods for Card class

--- models.py
+++ models.py
@@ -1,5 +1,34 @@
+colors = ['heart', 'diamonds', 'spades', 'clubs']
+
+signs = ['3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '2']
+
+
 class Deck:
     pass
 
+
 class Card:
-    pass
+    def __init__(self, sign, color):
+        self.sign = sign
+        self.color = color
+        self.value = signs.index(sign)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+
