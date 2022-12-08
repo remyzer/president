557aea640bc6e693d16eefa2a97df8eeb395adf2
Emmanuel Crenn <manucrenn@gmail.com>
2022-12-08 22:18:50 +0100

    relecture code quelque petites optimisation

--- models.py
+++ models.py
@@ -55,7 +55,7 @@ class Card:
         return self.value != other.value
 
     def __str__(self):
-        return "[{x},{y}]".format(x=self.sign, y=self.color)
+        return f"[{self.sign},{self.color}]"
 
 
 class PresidentGame:
@@ -135,17 +135,6 @@ class Player:
         """
         self.hand = sorted(self.hand)
 
-    @staticmethod
-    def check_if_multiple_cards_are_equals(cards):
-        """
-        Vérifie si les cartes jouées en même temps sont bien les mêmes
-        :param cards: List of Cards
-        """
-        previous_card = cards[0].value
-        for card in cards:
-            if card.value != previous_card.value:
-                raise CardsNotEqual("Les cartes jouées ne sont pas égales")
-
     def display_hand(self):
         """
         Affiche dans la console la main du joueur
@@ -165,24 +154,22 @@ class Player:
         cards_to_played = []
         last_trick = ""
         for card in last_cards_play:
-            last_trick += "[{x},{y}]".format(x=card.sign, y=card.color)
+            last_trick += f"[{card.sign},{card.color}]"
         print("Dernière cartes jouées : " + last_trick)
         self.display_hand()
         if len(last_cards_play) == 0:
             number_of_cards_to_play = int(input("Combien de Cartes voulez vous jouer?"))
         else:
             number_of_cards_to_play = len(last_cards_play)
-        for index in range(number_of_cards_to_play):
+        for _ in range(number_of_cards_to_play):
             card_to_play = int(input("Quelle carte voulez vous jouer?(index du tableau -1 pour passer)"))
             if card_to_play == -1:
                 return []
-            else:
-                cards_to_played.append(self.hand[card_to_play])
+            cards_to_played.append(self.hand[card_to_play])
         if cards_to_played.count(cards_to_played[0]) == len(cards_to_played): #On vérifie que tous les élément de la liste sont de même valeur
             if len(last_cards_play) == 0 or cards_to_played[0] >= last_cards_play[0]: #On vérifie que les cartes sélectionner à jouer sont valable en fonction de ce qui a été joué avant
                 for card in cards_to_played:
                     self.remove_from_hand(card)
-                    self.display_hand()
                 return cards_to_played
         return []
 
@@ -202,25 +189,15 @@ class Round:
             else:
                 number_of_players_to_pass = 0
                 self.last_cards_played = cards_play
-            #if self.last_cards_played[0].value == 12:
-                #break
+            if self.last_cards_played[0].value == 12:
+                break
             self.next_player = self.find_next_player()
         return self.next_player
-
-
-    #def next(self, cards_played):
-     #   self.last_cards_played = []
-     #   for card in cards_played:
-        #    self.cards_played.append(card)
-           # self.last_cards_played.append(card)
-
-      #  self.find_next_player()
 
     def find_next_player(self):
         if self.players.index(self.next_player) == len(self.players) - 1:
             return self.players[0]
-        else:
-            return self.players[self.players.index(self.next_player)+1]
+        return self.players[self.players.index(self.next_player)+1]
 
 
 class AIPlayer(Player):
@@ -234,20 +211,12 @@ class AIPlayer(Player):
         number_of_card_to_play = len(last_cards_play)
         match number_of_card_to_play:
             case 0:
-                if len(self.hand) == 0:
-                    return []
-                else:
-                    return [self.hand[0]]
+                card_to_play = []
+                if len(self.hand) != 0:
+                    card_to_play = [self.hand[0]]
+                return card_to_play
             case 1:
-                if len(self.hand) == 0:
-                    return []
-                else:
-                    for card in self.hand:
-                        if card >= last_cards_play[0]:
-                            cards = [card]
-                            self.remove_from_hand(card)
-                            return cards
-                    return []
+                return self.first_card_playable(last_cards_play)
             case 2:
                 return self.first_pair_playable(last_cards_play)
             case 3:
@@ -257,12 +226,30 @@ class AIPlayer(Player):
             case _:
                 return []
 
+    def first_card_playable(self, last_cards_play):
+        """
+        choisi la plus faible carte à jouer
+        :param last_cards_play: liste de une carte
+        :return: liste de deux cartes ou liste vide si jamais il n'y a pas de pair jouable
+        """
+        cards = []
+        if len(self.hand) == 0:
+            return []
+        for card in self.hand:
+            if card >= last_cards_play[0]:
+                cards = [card]
+            self.remove_from_hand(card)
+            return cards
+        return []
+
     def first_pair_playable(self, last_cards_play):
         """
         choisi la plus faible pair à jouer
         :param last_cards_play: liste de deux cartes
         :return: liste de deux cartes ou liste vide si jamais il n'y a pas de pair jouable
         """
+        if len(self.hand) == 0:
+            return []
         index = 0
         card_to_play = []
         while index + 1 < len(self.hand):
@@ -281,6 +268,8 @@ class AIPlayer(Player):
         :param last_cards_play: liste de trois cartes
         :return: liste de trois cartes ou liste vide si jamais il n'y a pas de triple jouable
         """
+        if len(self.hand) == 0:
+            return []
         index = 0
         card_to_play = []
         while index + 2 < len(self.hand):
@@ -302,6 +291,8 @@ class AIPlayer(Player):
         :param last_cards_play: liste de quatres cartes
         :return: liste de quatre cartes ou liste vide si jamais il n'y a pas de carré jouable
         """
+        if len(self.hand) == 0:
+            return []
         index = 0
         card_to_play = []
         while index + 3 < len(self.hand):
